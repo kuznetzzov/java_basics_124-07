d18404ef73799a1073af63e784a7ac6e116be4c7
Maksim Kuznetsov <m.kuznetsov@opendgp.ru>
2023-06-25 16:13:21 +0300

    fix1

---
+++ src/com/company/lesson2/Calculator.java
@@ -0,0 +1,51 @@
+package com.company.lesson2;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public long sum(long a, long b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public long subtract(long a, long b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public long divide(long a, long b) {
+        return a / b;
+    }
+}
